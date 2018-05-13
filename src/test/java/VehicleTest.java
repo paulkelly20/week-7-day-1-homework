6fc22e7d48e4eb0718fdb0f1fa9000f7fc5a01de
Paul Kelly <paul.kelly20@gmail.com>
2018-05-13 19:18:26 +0100

    test for vehicle to be attacked passes

--- src/test/java/VehicleTest.java
+++ src/test/java/VehicleTest.java
@@ -7,11 +7,15 @@ public class VehicleTest {
 
     Tank tank;
     Helicopter helicopter;
+    Category2 category2;
+    Category4 category4;
 
     @Before
     public void before() {
         tank = new Tank(50, "Sherman", 10);
         helicopter = new Helicopter(100,"Lockheed", 10);
+        category2 = new Category2("Ragnarok", 100, 20);
+        category4 = new Category4("Leatherback", 100, 60);
 
     }
 
@@ -24,4 +28,15 @@ public class VehicleTest {
     public void getHelicopterHealth() {
         assertEquals(100, helicopter.getHealth());
     }
+
+
+    @Test
+    public void testHelicopterAttacked() {
+        category2.attack(helicopter);
+        assertEquals(80, helicopter.getHealth());
+    }
 }
+
+
+
+
