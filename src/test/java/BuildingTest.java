e587cf9dc16bc7ed35308abb217ff5138b263b14
Paul Kelly <paul.kelly20@gmail.com>
2018-05-13 19:25:49 +0100

    test for building being attacked passes

---
+++ src/test/java/BuildingTest.java
@@ -0,0 +1,29 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class BuildingTest {
+
+
+    Tank tank;
+    Helicopter helicopter;
+    Category2 category2;
+    Category4 category4;
+    Building building;
+
+    @Before
+    public void before() {
+        tank = new Tank(50, "Sherman", 10);
+        helicopter = new Helicopter(100,"Lockheed", 10);
+        category2 = new Category2("Ragnarok", 100, 20);
+        category4 = new Category4("Leatherback", 100, 60);
+        building = new Building(100, "Chrystler", "Skyscraper");
+    }
+
+    @Test
+    public void testBuildingCanBeAttacked() {
+        category4.attack(building);
+        assertEquals(40, building.getHealth());
+    }
+}
